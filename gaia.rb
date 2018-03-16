c63b222d54cd0e7f4ce63e03487ca0e158985761
Alejandro Sabogal <asabogal.git@gmail.com>
2018-03-16 22:39:12 +0000

    automatically backed up by learn

--- gaia.rb
+++ gaia.rb
@@ -4,7 +4,7 @@ name = gets.to_s
 puts "Oh, that is a beautiful name!"
 sleep(2)
 
-puts "Tell me, in whay year where you born?"
+puts "Tell me, in what year where you born?"
 birth = gets.to_i
 
 puts "Wow! That's the year of the dragon!"
@@ -17,6 +17,6 @@ sleep(0.5)
 puts "..."
 sleep(0.5)
 puts "..."
-sleep(5)
+sleep(3)
 
 puts "You are #{Time.now.year - birth} years old (close enough)"
